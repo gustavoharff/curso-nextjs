c638e463e60364c2e84bbc068dff22a6a2e88278
Gustavo Harff <gustavo.harff@gmail.com>
2020-10-07 20:58:02 -0300

    styled components

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from '../styles/pages/Home'
+
+function Home() {
+  return (
+    <div>
+      <Title>Hello World</Title>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  color: #8257e5;
+`;
